f0042444b547698f2a928375c0e73d93c9a12a48
harshakomalla <harshakomalla@gmail.com>
2016-06-24 22:26:23 -0500

    Added code for parsing the results to Data frames
    Added HashingTF functionality to the existing code.

--- Source/ReDefine/src/main/scala/SparkMLLibPipeline.scala
+++ Source/ReDefine/src/main/scala/SparkMLLibPipeline.scala
@@ -23,7 +23,19 @@ object SparkMLLibpipeline {
       .setInputCol("words")
       .setOutputCol("filteredWords")
     val processedWordData= remover.transform(wordsData)
-    val r2=rescaledData.select("words","features")
-     spark.stop()
+    val hashingTF = new HashingTF()
+      .setInputCol("filteredWords").setOutputCol("rawFeatures").setNumFeatures(20)
+    val featurizedData = hashingTF.transform(processedWordData)
+    val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+    val idfModel = idf.fit(featurizedData)
+    val rescaledData = idfModel.transform(featurizedData)
+    //rescaledData.select("filteredWords","features", "sentence").take(10).foreach(println)
+    //rescaledData.orderBy(desc("words")).take(10).foreach(println)
+  val r2=rescaledData.select("words","features")
+    //rescaledData.sort($"features".desc).rdd.saveAsTextFile("topTFIDF")
+    //val rescaledData2=rescaledData.rdd
+    //rescaledData2.sortBy(we=>we,false)
+    //rescaledData.rdd.saveAsTextFile("tfidfpaper1output");
+    spark.stop()
   }
 }
